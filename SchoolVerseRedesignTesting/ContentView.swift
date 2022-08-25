6d5a5a463602eb7ca93a31f70c5742269f94c020
Daniel Shola-Philips <dsphilips18@gmail.com>
2022-08-25 11:12:51 -0400

    Initial Commit

---
+++ SchoolVerseRedesignTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SchoolVerseRedesignTesting
+//
+//  Created by Hackley on 8/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SchoolVerseRedesignTesting/SchoolVerseRedesignTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolVerseRedesignTestingApp.swift
+//  SchoolVerseRedesignTesting
+//
+//  Created by Hackley on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SchoolVerseRedesignTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
