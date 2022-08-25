f7d41887447ac85ebf03343c07ec7d95c51d02f1
Daniel Shola-Philips <dsphilips18@gmail.com>
2022-08-25 12:44:51 -0400

    Set up initial MVVM architecture for all preexisting folders, Set up main accent color assets

---
+++ SchoolVerseRedesignTesting/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  AppView.swift
+//  SchoolVerseRedesignTesting
+//
+//  Created by Hackley on 8/25/22.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
---
+++ SchoolVerseRedesignTesting/RouterView.swift
@@ -0,0 +1,20 @@
+//
+//  RouterView.swift
+//  SchoolVerseRedesignTesting
+//
+//  Created by Hackley on 8/25/22.
+//
+
+import SwiftUI
+
+struct RouterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RouterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RouterView()
+    }
+}
